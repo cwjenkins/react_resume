b697dfa5fe3476b02ee4c743580a42ad5e9e9d82
rinzler <jenkinscolton7@gmail.com>
2016-06-06 18:17:26 -0400

    Add meta viewport and throw in a comma

--- src/about_me.js
+++ src/about_me.js
@@ -8,7 +8,7 @@ var AboutMe = React.createClass({
               on the frontend with jQuery, but leveraging one of the many Javascript frameworks is still a work in progress
               (angular/react). Given my previous positions I can speak manager, developer, and client. I excel in both team and
               self-driven environments. I always go the extra mile for the client or company to ensure they are satisfied with
-              both the quality and timeliness of my work. When I’m not performing magic I’m a father who enjoys reading about
+              both the quality and timeliness of my work. When I’m not performing magic, I’m a father who enjoys reading about
               upcoming technologies and dabbling with new frameworks.
             </div>
       </div>
